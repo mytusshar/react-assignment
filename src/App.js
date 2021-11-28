56f730f6327b5f478426c6d1c269100e0085116d
mytusshar <mytusshar@gmail.com>
2021-11-28 21:31:06 +0530

    Added react assignment code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function TableComp(props) {
+	return (
+		<table className="Div">
+			<thead>
+				<tr>
+					<th id="table-head">Username</th>
+					<th id="table-head">Lastname</th>
+					<th id="table-head">Mobile</th>
+				</tr>
+			</thead>
+			<tbody>
+				{props.userData.map((o, i) => {
+					return (
+						<tr key={i} id="table-cell">
+							<td>{o.username}</td>
+							<td>{o.lastname}</td>
+							<td>{o.mobile}</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+}
+
+function FormComp(props) {
+	const [username, setName] = useState("Ross");
+	const [lastname, setLastName] = useState("Geller");
+	const [mobile, setMobileNum] = useState("99999999999");
+
+	const submitForm = (e) => {
+		e.preventDefault();
+		const user = {
+			username,
+			lastname,
+			mobile
+		};
+		props.setUserData([...props.userData, user]);
+	};
+	return (
+		<form className="Form" onSubmit={(e) => submitForm(e)}>
+			<input
+				id="input"
+				name="username"
+				value={username}
+				onChange={(e) => setName(e.target.value)}
+			></input>
+			<input
+				id="input"
+				name="lastname"
+				value={lastname}
+				onChange={(e) => setLastName(e.target.value)}
+			></input>
+			<input
+				id="input"
+				name="mobile"
+				value={mobile}
+				onChange={(e) => setMobileNum(e.target.value)}
+			></input>
+
+			<button type="submit" id="button">
+				Submit
+			</button>
+		</form>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [userData, setUserData] = useState([]);
+	return (
+		<div className="App">
+			<FormComp userData={userData} setUserData={setUserData} />
+			<TableComp userData={userData} />
+		</div>
+	);
 }
 
 export default App;
